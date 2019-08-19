418a433f67a9846231a2e2a18e8717e2aaf479af
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-08-19 13:08:31 +0200

    Add an example with Maybe

--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
@@ -3,12 +3,12 @@ package com.github.plokhotnyuk.jsoniter_scala.examples
 import com.github.plokhotnyuk.jsoniter_scala.core._
 import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.github.plokhotnyuk.jsoniter_scala.macros.JsonCodecMaker._
-import com.github.plokhotnyuk.jsoniter_scala.examples.Nullable._
+import com.github.plokhotnyuk.jsoniter_scala.examples.Maybe._
 
-final class Nullable[+A <: AnyRef] private(private val value: A) extends AnyVal {
-  def isEmpty: Boolean = value eq null
-
+final class Maybe[+A <: AnyRef] private(private val value: A) extends AnyVal {
   def isDefined: Boolean = value ne null
+
+  def isEmpty: Boolean = value eq null
 
   def get: A =
     if (value ne null) value
@@ -18,41 +18,45 @@ final class Nullable[+A <: AnyRef] private(private val value: A) extends AnyVal 
     if (value ne null) value
     else default
 
-  def flatMap[B <: AnyRef](f: A => Nullable[B]): Nullable[B] =
-    if (value ne null) f(value)
-    else new Nullable(null.asInstanceOf[B])
+  def getOrNull: A = value
 
-  def map[B <: AnyRef](f: A => B) : Nullable[B] = new Nullable({
+  def flatMap[B <: AnyRef](f: A => Maybe[B]): Maybe[B] =
+    if (value ne null) f(value)
+    else new Maybe(null.asInstanceOf[B])
+
+  def map[B <: AnyRef](f: A => B) : Maybe[B] = new Maybe({
     if (value ne null) f(value)
     else null.asInstanceOf[B]
   })
 
-  def toOption: Option[A] = if (value ne null) new Some(value) else None
+  def toOption: Option[A] =
+    if (value ne null) new Some(value)
+    else None
 
-  override def toString: String = s"Nullable($value)"
+  override def toString: String = s"Maybe($value)"
 }
 
-object Nullable {
-  def none[A <: AnyRef]: Nullable[A] = new Nullable(null.asInstanceOf[A])
+object Maybe {
+  def none[A <: AnyRef]: Maybe[A] = new Maybe(null.asInstanceOf[A])
 
-  implicit def apply[A <: AnyRef](value: A): Nullable[A] = new Nullable(value)
+  implicit def apply[A <: AnyRef](value: A): Maybe[A] = new Maybe(value)
 
-  def unapply[A <: AnyRef](n: Nullable[A]): Option[A] = n.toOption
+  def unapply[A <: AnyRef](n: Maybe[A]): Option[A] = n.toOption
 
-  def toNullableCodec[A <: AnyRef](codec: JsonValueCodec[A]): JsonValueCodec[Nullable[A]] =
-    new JsonValueCodec[Nullable[A]] {
-      override def decodeValue(in: JsonReader, default:  Nullable[A]): Nullable[A] =
+  def toNullableCodec[A <: AnyRef](codec: JsonValueCodec[A]): JsonValueCodec[Maybe[A]] =
+    new JsonValueCodec[Maybe[A]] {
+      override def decodeValue(in: JsonReader, default:  Maybe[A]): Maybe[A] =
         if (in.isNextToken('n')) in.readNullOrError(default, "expected JSON value")
         else {
           in.rollbackToken()
-          new Nullable(codec.decodeValue(in, default.value))
+          new Maybe(codec.decodeValue(in, default.value))
         }
 
-      override def encodeValue(x: Nullable[A], out: JsonWriter): Unit =
+      override def encodeValue(x: Maybe[A], out: JsonWriter): Unit =
         if (x == nullValue) out.writeNull()
         else codec.encodeValue(x.value, out)
 
-      override val nullValue: Nullable[A] = new Nullable(null.asInstanceOf[A])
+      override val nullValue: Maybe[A] = new Maybe(null.asInstanceOf[A])
     }
 }
 
@@ -60,11 +64,11 @@ object Nullable {
   * Example of basic usage from README.md
   */
 object Example01 {
-  case class Device(id: Int, model: Nullable[String])
+  case class Device(id: Int, model: Maybe[String])
 
   case class User(name: String, devices: Seq[Device])
 
-  implicit val nullableStringCodec: JsonValueCodec[Nullable[String]] = toNullableCodec(make(CodecMakerConfig()))
+  implicit val nullableStringCodec: JsonValueCodec[Maybe[String]] = toNullableCodec(make(CodecMakerConfig()))
   implicit val codec: JsonValueCodec[User] = make(CodecMakerConfig())
 
   def main(args: Array[String]): Unit = {
