64cc9a97e882d6719f82f674119f96833d0e3b01
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-12-13 18:50:01 +0100

    AppData example for https://stackoverflow.com/questions/59326794/spark-scala-parse-json-file-contains-only-list-of-string

--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Example01.scala
@@ -1,23 +1,24 @@
 package com.github.plokhotnyuk.jsoniter_scala.examples
 
-import com.github.plokhotnyuk.jsoniter_scala.macros._
-import com.github.plokhotnyuk.jsoniter_scala.core._
+case class AppData(apps :List[(Int, String)]) extends AnyVal
 
-/**
-  * Example of basic usage from README.md
-  */
 object Example01 {
-  case class Device(id: Int, model: String)
-
-  case class User(name: String, devices: Seq[Device])
-
-  implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make(CodecMakerConfig)
-
   def main(args: Array[String]): Unit = {
-    val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
-    val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
+    import java.io._
 
-    println(user)
-    println(new String(json, "UTF-8"))
+    import com.github.plokhotnyuk.jsoniter_scala.macros._
+    import com.github.plokhotnyuk.jsoniter_scala.core._
+
+    implicit val codec: JsonValueCodec[AppData] = JsonCodecMaker.make(CodecMakerConfig)
+
+    val filePath = "/tmp/app_data.json"
+    val out = new FileOutputStream(filePath)
+    try writeToStream[AppData](AppData(List((1, "a"), (2, "b"))), out)
+    finally out.close()
+    val in = new FileInputStream(filePath)
+    val appData =
+      try readFromStream[AppData](in)
+      finally in.close()
+    println(appData)
   }
 }
